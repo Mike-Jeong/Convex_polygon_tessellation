5387eb313fe5d38060c131e43b77d6575e29695a
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-10-26 23:05:34 +0900

    fix some codes in the DrawPolygon

--- src/DrawPolygon.java
+++ src/DrawPolygon.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- *   @Minsik Jeong (21135840)
- */
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
